2c51da8af857896fe8f80901e61bd110aaae513f
michael <mik.akamatsu@gmail.com>
2021-12-07 14:21:47 +0900

    change name

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Mikio'
+const name = 'Mikio (Michael)'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
